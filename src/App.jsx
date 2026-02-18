06c82d2dda24036dfd354a2a8495b320c9ddf50e
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-02-18 15:52:45 +1100

    button-mobileview-done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import ButtonGradient from "./assets/svg/ButtonGradient"
 import Button from "./components/Button"
+import Header from "./components/Header"
 
 function App() {
 
 
   return (
     <>
-      <div className='text-3xl font-bold underline'>
-        hello world!</div>
         <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-      <Button className="mt-10">
-          Something
-        </Button>
-        <ButtonGradient/>
+     
+          <Header />
         </div>
+       <ButtonGradient/>
        
       
     </>
