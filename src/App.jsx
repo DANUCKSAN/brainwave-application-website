2e5b8ea0224d6c7a19141d7cdb68203c1856adeb
DANUCKSAN <sathiyarajdanucksan@gmail.com>
2026-02-26 12:22:11 +1100

    done-last-part-AI-help

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Benefits from "./components/Benefits"
 import Collaboration from "./components/Collaboration"
 import Services from "./components/Services"
 import Pricing from "./components/Pricing"
+import Roadmap from "./components/Roadmap"
+import Footer from "./components/Footer"
 
 function App() {
 
@@ -20,6 +22,8 @@ function App() {
           <Collaboration/>
           <Services/>
           <Pricing/>
+          <Roadmap/>
+          <Footer/>
         </div>
        <ButtonGradient/>
        
